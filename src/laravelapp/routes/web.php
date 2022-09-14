4607fc9e6e2f490bd92a77ff28dd22084dff7cdd
amotsuka <am_otsuka@satt.jp>
2022-09-14 12:44:12 +0900

    dbの変更

--- src/laravelapp/routes/web.php
+++ src/laravelapp/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/timeline', 'App\Http\Controllers\Auth\TimelineController@showTimelinePage');   // <--- 追加
+Route::post('/timeline', 'App\Http\Controllers\Auth\TimelineController@postTweet');    // <--- 追加
